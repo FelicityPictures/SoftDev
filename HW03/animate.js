d6d63fd654337d0f19abae578b4f09bad2948801
FelicityPictures <felicitypictures@yahoo.com>
2016-02-23 12:04:46 -0500

    added hw03 folder

---
+++ HW03/animate.js
@@ -0,0 +1,37 @@
+var radius=0;
+var grow=true;
+var canvas=document.getElementById("canvas");
+var c=canvas.getContext("2d");
+
+c.fillStyle="#00BFFF";
+c.strokeStyle="#2E9AFE";
+
+function clear(){
+    c.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function draw(){
+    clear();
+    c.beginPath();
+    c.arc(canvas.width/2,canvas.height/2,radius,0,2*Math.PI);
+    c.fill();
+    c.stroke();
+    c.closePath();
+    if(grow==true && radius<canvas.width/2 && radius<canvas.height/2){
+        radius=radius+1;
+        console.log("radius++");
+    }else{
+        if(radius==canvas.width/2 || radius==canvas.height/2){
+            grow=false;
+            console.log("grow=false");
+        }
+        if(radius==0){
+            grow=true;
+            radius=2;
+        }
+        radius=radius-1;
+        console.log("radius--");
+    }
+    window.requestAnimationFrame(draw);
+}
+document.getElementById("start").addEventListener("click",draw);
