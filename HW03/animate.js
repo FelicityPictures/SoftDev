b5a9fde4fb6f424f808884863ed1b01fd7246550
FelicityPictures <felicitypictures@yahoo.com>
2016-02-23 20:31:09 -0500

    finished HW03

--- HW03/animate.js
+++ HW03/animate.js
@@ -1,19 +1,12 @@
 var canvas=document.getElementById("canvas");
 var c=canvas.getContext("2d");
-var logo=newImage().src="logo_dvd.jpg";
+var logo=new Image(600,400)
+logo.src="logo_dvd.jpg";
 var x=200;
 var y=0;
-/*
-1=up right
-2=down right
-3=down left
-4=up left
-*/
-var direction=2;
+var up = false;
+var right = true;
 var requestId;
-
-c.fillStyle="#00BFFF";
-c.strokeStyle="#2E9AFE";
 
 function clear(){
     c.clearRect(0, 0, canvas.width, canvas.height);
@@ -24,36 +17,29 @@ function draw(){
     c.beginPath();
     c.drawImage(logo,x,y,60,40);
     c.closePath();
-    if(direction==2){
-	x=x+1;
-	y=y+1;
+    if(up==true){
+        y=y-1;
     }else{
-	if(direction==3){
-	x=x-1;
-	y=y+1;
-	}else{
-	    if(direction==4){
-		x=x+1;
-		y=y-1;
-	    }else{
-		if(durection==1){
-		    x=x-1;
-		    y=y-1;
-		}
-	    }
-	}
+        y=y+1;
     }
-    if(y==canvas.height){
-	direction=4;
+    if(right==true){
+        x=x+1;
+    }else{
+        x=x-1;
     }
-    if(x==canvas.width){
-	direction=3;
+    if(y>=canvas.height-40){
+        up=true;
+    }else{
+        if(y<=0){
+            up=false;
+        }
     }
-    if(y==0){
-	direction=2;
-    }
-    if(x==0){
-	direction=1;
+    if(x>=canvas.width-60){
+        right=false;
+    }else{
+        if(x<=0){
+            right=true;
+        }
     }
     requestId=window.requestAnimationFrame(draw);
 }
