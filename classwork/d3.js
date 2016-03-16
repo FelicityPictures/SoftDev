8cd9064e136be9ecf6328a6833f236dd243453df
FelicityPictures <felicitypictures@yahoo.com>
2016-03-16 11:31:45 -0400

    d3 stuff works

--- classwork/d3.js
+++ classwork/d3.js
@@ -1,3 +1,6 @@
-document.selectAll("p").style("color", function(d, i) {
-  return i % 2 ? "grey" : "green";
+d3.selectAll("p").style("color", function(d, i) {
+  return i % 2 ? "pink" : "green";
 });
+
+var h = d3.layout.histogram();
+h.histogram(x[dog,cat,fish]);
