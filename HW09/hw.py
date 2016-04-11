9a7272a3b809c261bb6c22af5985f12f0f7ef99f
FelicityPictures <felicitypictures@yahoo.com>
2016-04-11 11:33:25 -0400

    HW09

--- HW09/hw.py
+++ HW09/hw.py
@@ -1,16 +1,30 @@
+def make_bold(fn):
+    return lambda : "<b>" + fn() + "</b>"
+
+def make_italic(fn):
+    return lambda : "<i>" + fn() + "</i>"
+
+@make_bold
+@make_italic
+def hello():
+    return "hello world"
+
+helloHTML = hello()
+
+print helloHTML
+#-----------------------------------------------------------#
 import time
 
-def tiempo(x):
-    return lambda: time.time()
+def track(x):
+    t = time.time()
+    def inner(*arg):
+        return x.func_name + "(" +  x(*arg) + ")"
+    runtime = time.time()-t
+    return inner + "time: " + runtime
 
-def fxName(x):
-    return lambda: x.func_name
-
-@tiempo
-#tiempo would be applied but not fxName
-@fxName
-def double():
-    return 22
+@track
+def double(string):
+    return string*2
 
 test = double()
 print test
